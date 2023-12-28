a2b68012e32c7cf45042ce310d54eea7683423d4
rimusophie <rimusophie@gmail.com>
2023-12-28 22:54:31 +0900

    テンプレートbladeを作成

--- config/app.php
+++ config/app.php
@@ -230,6 +230,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Constants' => App\Util\Constants::class,   // Constantsクラス
     ],
 
 ];
