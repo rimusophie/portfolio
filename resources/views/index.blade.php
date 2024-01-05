ced8a0ac8517ac13a2bbeb3fa18391290f586f32
rimusophie <rimusophie@gmail.com>
2024-01-05 20:32:02 +0900

    laravel10で作成

--- resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends(Constants::LAYOUT_FILE_BASE)
-@section(Constants::LAYOUT_SECTION_TITLE, 'rimusophie - ポートフォリオ')
-@section(Constants::LAYOUT_SECTION_MAIN)
-<div>
-    <ul>
-        <li><a href="/skillsheet-index">スキルシート詳細一覧</a></li>
-    </ul>
-</div>
--- resources/views/skillsheet-index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends(Constants::LAYOUT_FILE_BASE)
-@section(Constants::LAYOUT_SECTION_TITLE, 'rimusophie - ポートフォリオ')
-@section(Constants::LAYOUT_SECTION_MAIN)
-<div>
-    あああ
-</div>
